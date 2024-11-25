edb101c899cba53b0c673e917b3b09ff6da59363
manba024 <33890542+manba024@users.noreply.github.com>
2024-11-25 22:39:28 +0800

    Update options.lua
    将缩进设为4个空格

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+
+local opt = vim.o
+
+-- 设置缩进为 4 个空格
+opt.tabstop = 4 -- 一个制表符显示为 4 个空格
+opt.shiftwidth = 4 -- 自动缩进时使用 4 个空格
+opt.expandtab = true -- 用空格替代 Tab
+opt.softtabstop = 4 -- 编辑时按 Tab 键等价于 4 个空格
